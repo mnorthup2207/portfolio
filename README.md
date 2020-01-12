c5a05fc3043f5939f8e48002e6d5017ee50d67af
McCabe Northup <mccabenorthup@gmail.com>
2020-01-12 09:55:11 -0700

    landing page is in place, social media tabs functional, resume not yet wired, page is a bit buggy whenloading

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # portfolio
+
+## Credits 
+Landing Page Photo by Brian Patrick Tagalog on Unsplash https://unsplash.com/@briantagalog?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge
+
+Webpage Layout is using Skeleton rows and columns
+
+
+
+
---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var heading = document.querySelector('.headingDiv');
+var navigation = document.querySelector('.navDiv');
+var home = document.querySelector('.homeNav')
+var portfolio = document.querySelector('.portNav')
+var contacts = document.querySelector('.contNav')
+var linked = document.querySelector('.linked');
+var pdf = document.querySelector('.resume');
+var github = document.querySelector('.github');
+
+// Nav Bar
+function toggleDown() {
+    console.log('yippe');
+    navigation.classList.toggle('open');
+};
+heading.addEventListener('click', toggleDown)
+
+
+// Social Media and resume
+function linkedIn() {
+    window.open("https://www.linkedin.com/in/mccabe-northup-2b76b885/", "_blank")
+};
+function resume() {
+    window.open('', "_blank")
+};
+function git() {
+    window.open('https://github.com/mnorthup2207', "_blank")
+};
+linked.addEventListener('click', linkedIn)
+github.addEventListener('click', git)
